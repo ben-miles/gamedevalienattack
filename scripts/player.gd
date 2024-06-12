7920eda7f6d570cd1f071cbc7b530bf5edf5b37b
Ben Miles <bcgm3studios@gmail.com>
2024-06-11 23:28:01 -0400

    Add Movement Inputs

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,14 @@
 extends CharacterBody2D
+
+func _physics_process(delta):
+	velocity = Vector2(0,0)
+	var force = 250
+	if Input.is_action_pressed("player_move_down"):
+		velocity.y = force
+	if Input.is_action_pressed("player_move_left"):
+		velocity.x = -force
+	if Input.is_action_pressed("player_move_right"):
+		velocity.x = force
+	if Input.is_action_pressed("player_move_up"):
+		velocity.y = -force
+	move_and_slide()
