7eeda7c90c7bededb72564a71b800b4316e41793
Ben Miles <bcgm3studios@gmail.com>
2024-06-11 23:33:51 -0400

    Prevent Player from Leaving Window

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	velocity = Vector2(0,0)
-	var force = 250
+	var force = 300
+	
 	if Input.is_action_pressed("player_move_down"):
 		velocity.y = force
 	if Input.is_action_pressed("player_move_left"):
@@ -11,4 +12,16 @@ func _physics_process(delta):
 		velocity.x = force
 	if Input.is_action_pressed("player_move_up"):
 		velocity.y = -force
+	
 	move_and_slide()
+	
+	if global_position.x < 0:
+		global_position.x = 0
+	if global_position.x > 1280:
+		global_position.x = 1280
+	if global_position.y < 0:
+		global_position.y = 0
+	if global_position.y > 720:
+		global_position.y = 720
+		
+	print(global_position)
