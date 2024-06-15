08c02793b33bbb607290563565cc82dda29c5d5e
Ben Miles <bcgm3studios@gmail.com>
2024-06-15 12:29:32 -0400

    Spawn Enemies At Random Preset Points

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
+@onready var spawn_positions = $SpawnPositions
+
 func _on_timer_timeout():
-	print("timeout")
+	enemy_spawn()
+
+func enemy_spawn():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spwan_position = spawn_positions_array.pick_random()
+	
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_instance.global_position = random_spwan_position.global_position
+	add_child(enemy_instance)
