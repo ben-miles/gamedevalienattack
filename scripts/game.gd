f99bc0f47d1db1d0ebd3508898f8f4ca70c20ad7
Ben Miles <bcgm3studios@gmail.com>
2024-06-16 01:57:59 -0400

    Increase Score on Enemy Death

--- scripts/game.gd
+++ scripts/game.gd
@@ -17,4 +17,9 @@ func _on_player_took_damage():
 		print(lives)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
+	enemy_instance.connect("died", _on_enemy_died)
 	add_child(enemy_instance)
+
+func _on_enemy_died():
+	score += 100
+	print(score)
