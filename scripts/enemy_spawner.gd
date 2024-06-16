cd5884dfd490d3b486335466eaa89aeb36c6d141
Ben Miles <bcgm3studios@gmail.com>
2024-06-16 13:45:29 -0400

    Fix Typo in Var Name in Enemy Spawner

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -11,8 +11,8 @@ func _on_timer_timeout():
 
 func enemy_spawn():
 	var spawn_positions_array = spawn_positions.get_children()
-	var random_spwan_position = spawn_positions_array.pick_random()
+	var random_spawn_position = spawn_positions_array.pick_random()
 	
 	var enemy_instance = enemy_scene.instantiate()
-	enemy_instance.global_position = random_spwan_position.global_position
+	enemy_instance.global_position = random_spawn_position.global_position
 	emit_signal("enemy_spawned", enemy_instance)
