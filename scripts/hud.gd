b38cab09f7ae3fcf3b4d366b469268f335e8e078
Ben Miles <bcgm3studios@gmail.com>
2024-06-16 13:26:01 -0400

    Show Player Lives Remaining

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var score = $Score
+@onready var lives_left = $LivesLeft
 
 func set_score_label(new_score):
 	score.text = "Score: " + str(new_score)
+
+func set_lives(amount):
+	lives_left.text = "X " + str(amount)
