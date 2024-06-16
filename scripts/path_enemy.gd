e9fc916a9f85b0b8b4f7ab410d06dac2156bbd37
Ben Miles <bcgm3studios@gmail.com>
2024-06-16 19:20:36 -0400

    Fix for Path Enemy Not Despawning

--- scripts/path_enemy.gd
+++ scripts/path_enemy.gd
@@ -1,11 +1,14 @@
 extends Path2D
 
 @onready var path_follow = $PathFollow2D
+@onready var enemy = $PathFollow2D/Enemy
 
 func _ready():
 	path_follow.set_progress_ratio(1)
 
 func _process(delta):
 	path_follow.progress_ratio -= 0.35 * delta
-	if(path_follow.progress_ratio <= 0):
+	print(path_follow.progress_ratio)
+	if(path_follow.progress_ratio <= 0.1):
 		queue_free()
+		print('path enemy despawned')
